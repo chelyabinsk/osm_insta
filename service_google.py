47dc1d54a0fb7fc457ccd6582024c55d5f4e9d55
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2019-08-03 23:00:48 +0100

    Update service_google.py

--- service_google.py
+++ service_google.py
@@ -48,7 +48,7 @@ class GDrive():
         data["client_email"] = os.environ["GDRIVE_CLIENT_EMAIL"]
         data["client_id"] = os.environ["GDRIVE_CLIENT_ID"]
         data["client_x509_cert_url"] = os.environ["GDRIVE_CERT_URL"]
-        print("daaaaaaaata {}".format(data))
+        print("daaaaaaaata {}".format(data["private_key"]))
         with open(filename, 'w') as f:
             json.dump(data, f)
     
