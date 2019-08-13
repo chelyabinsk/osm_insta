b275bf4ec4b1d69e64b9f6948fbd21d424786f7d
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2019-08-13 20:01:40 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ import main
 import os,shutil
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+print("HIIIIIIIIIIIIIII")
 main.main()
 sched = BlockingScheduler()
 
@@ -27,7 +27,7 @@ def get_size(start_path = '.'):
             if not os.path.islink(fp):
                 total_size += os.path.getsize(fp)
     return total_size
-@sched.scheduled_job('interval', minutes=20)
+@sched.scheduled_job('interval', minutes=2)
 def timed_job():
     main.main()
     clean_up_tiles()
